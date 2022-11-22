f27167bb778e016c89e43430bf9b7baf110761f6
Eunji Lee <eunijcious@gmail.com>
2022-11-22 12:02:50 +0900

    make directory added

---
+++ make/diff.c
@@ -0,0 +1,7 @@
+extern int count;
+
+int get_diff(int a, int b)
+{
+	count++;
+	return a > b? a-b : b-a;
+}
---
+++ make/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int count;
+
+extern int get_sum(int, int);
+extern int get_diff(int, int);
+
+int main(int argc, char* argv[])
+{
+	if (argc < 3) {
+		printf("Usage: ./a.out 3 5\n");
+		return -1;
+	}
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+
+	printf("%d + %d = %d\n", a, b, get_sum(a, b));
+	printf("%d - %d = %d\n", a, b, get_diff(a, b));
+
+}
